33cf1d8de48ae4869a2a627811899835c50b6e63
MuratFurkanSen <muratfurkan.sen79@gmail.com>
2024-11-03 03:29:39 +0300

    Auth  Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
-import 'package:btk_hackathon/ui/uye_giris.dart';
+import 'package:btk_hackathon/widget_tree.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}):super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.orange,
       ),
-      home: UyeGiris(),
+      home: const WidgetTree(),
     );
   }
 }
