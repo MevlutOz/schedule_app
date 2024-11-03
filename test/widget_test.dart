f4d9adcfd398f0c2bc0277b8de26f1d651c39872
MuratFurkanSen <muratfurkan.sen79@gmail.com>
2024-11-03 04:14:56 +0300

    Conflict Fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:btk_hackathon/main.dart';
+import 'package:btk_hackathon/login_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
